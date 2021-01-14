5e3827b41ef59c2e54c8363893cb0873115f08f6
Luke Hyman <45041058+ljjh20@users.noreply.github.com>
2021-01-14 12:12:23 -0800

    window is split between parameters on left and a push button on right (#3)
    * window is split between parameters on left and a push button on right
    
    * added placeholder text to pushbutton and updated commentes

--- widgets/piscesInputParameterWidget.py
+++ widgets/piscesInputParameterWidget.py
@@ -14,7 +14,7 @@ class MainWidgetWindow(QMainWindow):
         self.title = "Pisces Parameter Controller"
         self.left = 10
         self.top = 10
-        self.width = 400
+        self.width = 800
         self.height = 500
         self.initUI()
 
@@ -25,52 +25,80 @@ class MainWidgetWindow(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         #  initialize layout
-        master_layout = QVBoxLayout()
-        master_layout.setAlignment(Qt.AlignTop)
+        window_layout = QHBoxLayout()
+
+        right_window_layout = QVBoxLayout()
+        right_window_layout.setAlignment(Qt.AlignTop)
+        right_window_layout.addWidget(QPushButton("Live Mode"))
+
+        left_window_layout = QVBoxLayout()
+        left_window_layout.setAlignment(Qt.AlignTop)
 
         #  add widgets to vertical master layout
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "exposure", 0.001, 1, float, 0.001, 0.02))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "nb_timepoints", 1, 10000, int, 1, 600))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "scan_step", 0.01, 1, float, 0.01, 0.1))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stage_scan_range", 0, 10000, float, 0.01, 1000))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "vertical_pixels", 0, 4000, int, 1, 2048))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "num_samples", 0, 100, int, 1, 20))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "offset_view1", 0, 3180, float, 0.1, 1550))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "offset_view2", 0, 3180, float, 0.1, 1650))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view1_galvo1", -10, 10, float, 0.01, 4.2))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view1_galvo2", -10, 10, float, 0.01, -4.08))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view2_galvo1", -10, 10, float, 0.01, -4.37))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view2_galvo2", -10, 10, float, 0.01, 3.66))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stripe_reduction_range", 0, 10, float, 0.01, 0.1))
-        master_layout.addWidget(custom_decorations.LineBreak())
-        master_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stripe_reduction_offset", -10, 10, float, 0.01, 0.58))
-        master_layout.addWidget(custom_decorations.LineBreak())
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "exposure", 0.001, 1, float, 0.001, 0.02))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
 
-        #  create base class container for layout and align it centrally
-        widget = QWidget()  # QWidget() acts as a container for the laid out widgets - base class for all widgets
-        widget.setLayout(master_layout)
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "nb_timepoints", 1, 10000, int, 1, 600))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
 
-        #  add scroll bar
-        scroll_bar = QScrollArea()
-        scroll_bar.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
-        scroll_bar.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        scroll_bar.setWidgetResizable(False)
-        scroll_bar.setStyleSheet("background : lightgray;")
-        scroll_bar.setWidget(widget)
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "scan_step", 0.01, 1, float, 0.01, 0.1))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
 
-        self.setCentralWidget(scroll_bar)
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stage_scan_range", 0, 10000, float, 0.01, 1000))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "vertical_pixels", 0, 4000, int, 1, 2048))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "num_samples", 0, 100, int, 1, 20))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "offset_view1", 0, 3180, float, 0.1, 1550))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "offset_view2", 0, 3180, float, 0.1, 1650))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view1_galvo1", -10, 10, float, 0.01, 4.2))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view1_galvo2", -10, 10, float, 0.01, -4.08))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view2_galvo1", -10, 10, float, 0.01, -4.37))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "view2_galvo2", -10, 10, float, 0.01, 3.66))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stripe_reduction_range", 0, 10, float, 0.01, 0.1))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        left_window_layout.addWidget(TextBox_and_Slider.InitializeSliderTextB(None, "stripe_reduction_offset", -10, 10, float, 0.01, 0.58))
+        left_window_layout.addWidget(custom_decorations.LineBreak())
+
+        #  create container for left_window_layout that can be contained by scroll bar widget
+        left_widget = QWidget()  # QWidget() acts as a container for the laid out widgets - base class for all widgets
+        left_widget.setLayout(left_window_layout)
+
+        #  add scroll bar area that contains left window widget
+        left_scroll_bar = QScrollArea()
+        left_scroll_bar.setMaximumWidth(self.width / 2)
+        left_scroll_bar.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        left_scroll_bar.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        left_scroll_bar.setWidgetResizable(True)
+        left_scroll_bar.setStyleSheet("background : lightgray;")
+        left_scroll_bar.setWidget(left_widget)
+
+        # add left and right window components to main window (left is a widget due to scroll bar)
+        window_layout.addWidget(left_scroll_bar)
+        window_layout.addLayout(right_window_layout)
+
+        # container for main window layout that can be set as central widget
+        mainWidget = QWidget()
+        mainWidget.setLayout(window_layout)
+
+        self.setCentralWidget(mainWidget)
 
 
 if __name__ == '__main__':
