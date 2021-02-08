52580b03a3e14403fd55007009926f7f3657b77c
Luke Hyman <ljjh20@bath.ac.uk>
2021-02-08 14:33:53 -0800

    NIDaq instance only launches on editingFinished event from spinbox or onMouseReleaseEvent of slider. Temporarily reimplemented placeholder thread that doesn't crash without Bin hardware for testing

--- widgets/gui/qt_nidaq_worker.py
+++ widgets/gui/qt_nidaq_worker.py
@@ -1,5 +1,6 @@
 import logging
 from PyQt5.QtCore import *
+import time
 
 from widgets.hardware.alternative_control import NIdaq
 
@@ -16,30 +17,37 @@ class NIDaqWorker(QRunnable):
         self.channel = int(channel)
         self.args = args
         self.kwargs = kwargs
+        self.thread_active = True
 
         self.daq_card = None
 
     @pyqtSlot()
     def run(self):
         logging.info(f"NIDaq Instance launched")
+        while True:
+            time.sleep(1)
+            logging.info(self.thread_active)
+            if not self.thread_active:
+                break
 
-        self.daq_card = NIdaq(exposure=self.parameters[0],
-                              nb_timepoints=self.parameters[1],
-                              scan_step=self.parameters[2],
-                              stage_scan_range=self.parameters[3],
-                              vertical_pixels=self.parameters[4],
-                              num_samples=self.parameters[5],
-                              offset_view1=self.parameters[6],
-                              offset_view2=self.parameters[7],
-                              view1_galvo1=self.parameters[8],
-                              view1_galvo2=self.parameters[9],
-                              view2_galvo1=self.parameters[10],
-                              view2_galvo2=self.parameters[11],
-                              stripe_reduction_range=self.parameters[12],
-                              stripe_reduction_offset=self.parameters[13])
-
-        self.daq_card.select_view(1)
-        self.daq_card.select_channel_remove_stripes(488)
+        #self.daq_card = NIdaq(exposure=self.parameters[0],
+        #                      nb_timepoints=self.parameters[1],
+        #                      scan_step=self.parameters[2],
+        #                      stage_scan_range=self.parameters[3],
+        #                      vertical_pixels=self.parameters[4],
+        #                      num_samples=self.parameters[5],
+        #                      offset_view1=self.parameters[6],
+        #                      offset_view2=self.parameters[7],
+        #                      view1_galvo1=self.parameters[8],
+        #                      view1_galvo2=self.parameters[9],
+        #                      view2_galvo1=self.parameters[10],
+        #                      view2_galvo2=self.parameters[11],
+        #                      stripe_reduction_range=self.parameters[12],
+        #                      stripe_reduction_offset=self.parameters[13])
+        #
+        #self.daq_card.select_view(1)
+        #self.daq_card.select_channel_remove_stripes(488)
 
     def stop(self):
-        self.daq_card.stop_now = True
+        #self.daq_card.stop_now = True
+        self.thread_active = False
