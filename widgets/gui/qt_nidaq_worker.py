edbbe6f5286cdc2438cea8be7419297caf16f443
Luke Hyman <ljjh20@bath.ac.uk>
2021-02-23 14:50:07 -0800

    added real calls to NIDaq back and commented out dummy to mimic shutdown delay

--- widgets/gui/qt_nidaq_worker.py
+++ widgets/gui/qt_nidaq_worker.py
@@ -29,5 +29,5 @@ class NIDaqWorker(QRunnable):
             self.signals.finished.emit()
 
     def stop(self):
-        # self.daq_card.stop_now = True
-        self.thread_running = False
+        self.daq_card.stop_now = True
+        #self.thread_running = False
