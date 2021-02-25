c9cf5d0a74545028b75a320587641da0a140e1d8
Luke Hyman <ljjh20@bath.ac.uk>
2021-02-25 12:30:21 -0800

    split live and timelapse into separate widgets. Use of dock splitting for smoother behaviour. Maximum sizes removed

--- widgets/gui/qt_main_window.py
+++ widgets/gui/qt_main_window.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import qdarkstyle
 from widgets.gui import qt_parameters_widget, qt_mode_widget
+from widgets.gui.qt_live_control import LiveControl
+from widgets.gui.qt_timelapse_control import TimelapseControl
 
 
 class MainWindow(QMainWindow):
@@ -19,25 +21,44 @@ class MainWindow(QMainWindow):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        self.mode_dock = QDockWidget("Mode Control", self)
-        self.mode_dock.setMaximumSize(200, 300)
-        self.mode_dock.setFeatures(QDockWidget.DockWidgetFloatable | QDockWidget.DockWidgetMovable)
-        self.mode_dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        self.central_widget = QWidget()
+        self.central_widget.hide()
+        self.setCentralWidget(self.central_widget)
 
-        self.mode_widget = qt_mode_widget.ModeWidget(self)
-        self.mode_dock.setWidget(self.mode_widget)
+        self.setDockOptions(QMainWindow.AnimatedDocks | QMainWindow.AllowNestedDocks)
 
-        self.addDockWidget(Qt.RightDockWidgetArea, self.mode_dock)
+        self.live_dock = QDockWidget("Live", self)
+        #self.live_dock.setMaximumSize(200, 300)
+        self.live_dock.setFeatures(QDockWidget.DockWidgetClosable | QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        #self.mode_dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+
+        #self.live_widget = qt_mode_widget.ModeWidget(self)
+        self.live_widget = LiveControl(self, "Live Mode")
+        self.live_dock.setWidget(self.live_widget)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.live_dock)
+
+        self.timelapse_dock = QDockWidget("Timelapse", self)
+        #self.timelapse_dock.setMaximumSize(200, 300)
+        self.timelapse_dock.setFeatures(
+            QDockWidget.DockWidgetClosable | QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        # self.mode_dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+
+        self.timelapse_widget = TimelapseControl(self, "Timelapse Mode")
+        self.timelapse_dock.setWidget(self.timelapse_widget)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.timelapse_dock)
 
         self.parameters_dock = QDockWidget("Parameters", self)
-        self.parameters_dock.setMaximumSize(500, 650)
-        self.parameters_dock.setFeatures(QDockWidget.DockWidgetFloatable | QDockWidget.DockWidgetMovable)
-        self.parameters_dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
+        #self.parameters_dock.setMaximumSize(500, 650)
+        self.parameters_dock.setFeatures(QDockWidget.DockWidgetClosable | QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetFloatable)
+        #self.parameters_dock.setAllowedAreas(Qt.LeftDockWidgetArea | Qt.RightDockWidgetArea)
 
         self.parameters_widget = qt_parameters_widget.ParametersWidget(self)
         self.parameters_dock.setWidget(self.parameters_widget)
-
         self.addDockWidget(Qt.LeftDockWidgetArea, self.parameters_dock)
+
+        self.splitDockWidget(self.parameters_dock, self.live_dock, Qt.Horizontal)
+
+        self.splitDockWidget(self.live_dock, self.timelapse_dock, Qt.Vertical)
 
         self.show()
 
