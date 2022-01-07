0d6ebb838ce8770bc1c3337defe1be1c9d252484
chanzuckerberg <asolak14@ku.edu.tr>
2022-01-07 11:14:55 -0800

    run method initial implementation is done, currently not saving any data nor streaming any data

--- copylot/hardware/hamamatsu_camera/camera.py
+++ copylot/hardware/hamamatsu_camera/camera.py
@@ -1,6 +1,56 @@
+from copylot.hardware.hamamatsu_camera.dcam import Dcamapi, Dcam
+
+
+class CameraException(Exception):
+    pass
+
+
 class Camera:
     def __init__(self, camera_index: int = 0):
         self._camera_index = camera_index
 
-    def run(self):
-        raise NotImplementedError
+    def run(self, nb_frame: int = 100000):
+        if Dcamapi.init():
+            dcam = Dcam(self._camera_index)
+            if dcam.dev_open():
+                nb_buffer_frames = 3
+                if dcam.buf_alloc(nb_buffer_frames):
+
+                    if dcam.cap_start():
+                        timeout_milisec = 20
+
+                        for _ in range(nb_frame):
+                            if dcam.wait_capevent_frameready(timeout_milisec):
+                                data = dcam.buf_getlastframedata()  # Data is here
+                            else:
+                                dcamerr = dcam.lasterr()
+                                if dcamerr.is_timeout():
+                                    print("===: timeout")
+                                else:
+                                    print(
+                                        f"dcam.wait_event() fails with error {dcamerr}"
+                                    )
+                                    break
+
+                        dcam.cap_stop()
+                    else:
+                        raise CameraException(
+                            f"dcam.cap_start() fails with error {dcam.lasterr()}"
+                        )
+
+                    dcam.buf_release()  # release buffer
+                else:
+                    raise CameraException(
+                        f"dcam.buf_alloc({nb_buffer_frames}) fails with error {dcam.lasterr()}"
+                    )
+                dcam.dev_close()
+            else:
+                raise CameraException(
+                    f"dcam.dev_open() fails with error {dcam.lasterr()}"
+                )
+        else:
+            raise CameraException(
+                f"Dcamapi.init() fails with error {Dcamapi.lasterr()}"
+            )
+
+        Dcamapi.uninit()
