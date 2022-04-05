67f663fdba8e49df7cd226ffc787653d218581bc
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-05 16:47:08 -0700

    implemented, tested, working

--- copylot/hardware/asi_stage/demo/axis_info.py
+++ copylot/hardware/asi_stage/demo/axis_info.py
@@ -1,10 +1,12 @@
 from copylot.hardware.asi_stage.stage import ASIStage
 
 
-def info_x_demo():
+def info_demo():
     stage = ASIStage()
-    stage.info_x()
+    stage.info("X")
+    print("======")
+    stage.info("Y")
 
 
 if __name__ == '__main__':
-    info_x_demo()
+    info_demo()
