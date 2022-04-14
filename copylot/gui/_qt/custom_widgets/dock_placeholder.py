f9ade930cd03dc8c0db59316156b27c50e2eedeb
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-14 16:42:53 -0700

    implemented

--- copylot/gui/_qt/custom_widgets/dock_placeholder.py
+++ copylot/gui/_qt/custom_widgets/dock_placeholder.py
@@ -1,19 +1,17 @@
 import importlib
-
 from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 
 from copylot.gui._qt import dockables
 
 
 class DockPlaceholder(QWidget):
-    def __init__(self, parent, dock, widget_to_load, widget_name, *args, **kwargs):
+    def __init__(self, parent, dock, widget_name, widget_init_args, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        get_widget_instance_from_name(parent, widget_name)
 
         self.parent = parent
         self.dock = dock
-        self.widget_to_load = widget_to_load
+        self.widget_name = widget_name
+        self.widget_init_args = widget_init_args
 
         self.placeholder_layout = QHBoxLayout()
         self.placeholder_layout.setContentsMargins(0, 0, 0, 0)
@@ -25,22 +23,31 @@ class DockPlaceholder(QWidget):
         self.setLayout(self.placeholder_layout)
 
     def load_widget(self):
-        self.dock.setWidget(self.widget_to_load)
+        print("load widget")
+        widget = get_widget_instance_from_name(self.widget_name, self.widget_init_args)
+        print("widget returned")
+        self.dock.setWidget(widget)
         self.parent.restoreDockWidget(self.dock)
 
 
-def get_widget_instance_from_name(parent, name: str):
+def get_widget_instance_from_name(name: str, widget_init_args):
+    print("begin")
     response = importlib.import_module(dockables.__name__ + '.' + name)
-    print(response)
-    print(dir(response))
-    # elem = [
-    #     x
-    #     for x in dir(response)
-    #     if (name.replace("_", "") + "Regressor").lower() in x.lower()
-    # ][
-    #     0
-    # ]  # class name
 
-    # elem_class = response.__getattribute__(elem)
-    #
-    # return elem_class(parent)
+    print(f"response {response}")
+    elem = [
+        x
+        for x in dir(response)
+        if (name.replace("_", "") + "DockWidget").lower() in x.lower()
+    ][
+        0
+    ]  # class name
+
+    print(elem)
+
+    elem_class = response.__getattribute__(elem)
+
+    print(elem_class)
+    print(*widget_init_args)
+
+    return elem_class(*widget_init_args)
