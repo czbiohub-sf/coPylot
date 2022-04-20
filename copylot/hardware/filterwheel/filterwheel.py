07ea42a417a268fdc0f0faaf978336634f381f41
AhmetCanSolak <asolak14@ku.edu.tr>
2022-04-20 16:45:04 -0700

    filterwheel methods implemented

--- copylot/hardware/filterwheel/filterwheel.py
+++ copylot/hardware/filterwheel/filterwheel.py
@@ -19,3 +19,20 @@ class FilterWheel:
         self.serial_connection.baudrate = 1000000
 
         print(self.serial_connection.name)
+
+    def __del__(self):
+        self.serial_connection.close()
+
+    def _send_message(self, message: str):
+        self.serial_connection.write(bytes(f"{message}\r", encoding="ascii"))
+
+    def _read_response(self) -> str:
+        return self.serial_connection.readline().decode(encoding="ascii")
+
+    def set_position(self, position_index: int):
+        if position_index < 0 or position_index > 5:
+            raise ValueError("Bad value for position_index argument...")
+
+        message = f"#s{position_index}"
+        self._send_message(message)
+        print(self._read_response())
