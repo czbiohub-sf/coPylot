aea9b427dc36989497a38cdacdd34d9674a3c30f
acs-ws <asolak@ku.edu.tr>
2022-05-11 16:06:20 -0700

    read config, test for read config, nb_devices property, name attribute, MicroscopeConfig class abstraction introduced

--- copylot/microscope_config/test/test_microscope_config.py
+++ copylot/microscope_config/test/test_microscope_config.py
@@ -1,9 +1,11 @@
-from copylot.microscope_config.microscope_config import read_config
+import pathlib
+
+from copylot.microscope_config.microscope_config import MicroscopeConfig
 
 
 def test_read_config():
-    config_path = "copylot/microscope_config/configs/daxi.yaml"
-    scope_config = read_config(config_path)
+    config_path = pathlib.Path("../configs/daxi.yaml").absolute()
+    scope_config = MicroscopeConfig.read_config(config_path)
 
     assert scope_config.name == "daxi"
     assert scope_config.nb_devices == 4
