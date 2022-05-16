ea744ed027bf723f8f9f8c89abef340d27738342
AhmetCanSolak <asolak14@ku.edu.tr>
2022-05-16 08:33:26 -0700

    microscope_config submodule is done

--- copylot/microscope_config/microscope_config.py
+++ copylot/microscope_config/microscope_config.py
@@ -2,16 +2,37 @@ import yaml
 
 
 class MicroscopeConfig:
+    """
+
+    Parameters
+    ----------
+    config_dict
+
+    """
     def __init__(self, config_dict: dict):
         self.name = config_dict['name']
         self.config_dict = config_dict
 
     @property
     def nb_devices(self):
+        """Returns number of devices"""
         return len(self.config_dict['hardware'])
 
     @staticmethod
-    def read_config(config_path):
+    def read_config(config_path: str):
+        """
+        This method creates and initializes a MicroscopeConfig
+        instance with given config yaml file.
+
+        Parameters
+        ----------
+        config_path : str
+
+        Returns
+        -------
+        MicroscopeConfig
+
+        """
         with open(config_path, "r") as stream:
             try:
                 response_dict = yaml.safe_load(stream)
