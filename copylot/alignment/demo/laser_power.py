f66b7f3e7bdaa812fc4e7debbbb07484392ae952
acs-ws <asolak@ku.edu.tr>
2022-05-31 15:16:40 -0700

    initial commit

---
+++ copylot/alignment/demo/laser_power.py
@@ -0,0 +1,15 @@
+from time import sleep
+
+
+def demo_laser_power():
+    laser = Laser()
+
+    for _ in range(5):
+        laser.set_power(0.01 * _)
+        laser.turn_on()
+        sleep(1)
+        laser.turn_off()
+
+
+if __name__ == '__main__':
+    demo_laser_power()
