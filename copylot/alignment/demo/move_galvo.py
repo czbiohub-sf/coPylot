0a281e6f93b650aae18723d4f0ac61a84d7a5a1e
acs-ws <asolak@ku.edu.tr>
2022-05-31 15:26:26 -0700

    initial move galvo demo is added

---
+++ copylot/alignment/demo/move_galvo.py
@@ -0,0 +1,14 @@
+from time import sleep
+
+
+def demo_move_galvo():
+    galvo = Galvo()
+
+    for _ in range(6):
+        galvo.set_position(15 * _)
+        sleep(1)
+        galvo.zero()
+
+
+if __name__ == '__main__':
+    demo_move_galvo()
