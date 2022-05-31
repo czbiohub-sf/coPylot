b4bbf88f93d8374d8e6940668ebc32f0aff199f8
acs-ws <asolak@ku.edu.tr>
2022-05-31 16:44:47 -0700

    line break classes refactored together

--- copylot/gui/_qt/dockables/laser.py
+++ copylot/gui/_qt/dockables/laser.py
@@ -1,6 +1,6 @@
 from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
 
-from copylot.gui._qt.custom_widgets.line_break import LineBreak
+from copylot.gui._qt.custom_widgets.line_break import QVLineBreakWidget
 
 
 class LaserDockWidget(QWidget):
@@ -13,7 +13,7 @@ class LaserDockWidget(QWidget):
         self.power_button.clicked.connect(self.power_button.toggle)
         self.main_layout.addWidget(self.power_button)
 
-        self.main_layout.addWidget(LineBreak())
+        self.main_layout.addWidget(QVLineBreakWidget(self))
 
         self.setLayout(self.main_layout)
 
