dfa2a2ebba952f259eb163911a3a404ff8dd3816
acs-ws <asolak@ku.edu.tr>
2022-06-02 13:06:04 -0700

    initial commit, slider added

--- copylot/gui/_qt/dockables/laser.py
+++ copylot/gui/_qt/dockables/laser.py
@@ -1,6 +1,5 @@
-from PyQt5.QtWidgets import QLabel, QLineEdit
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton, QGridLayout
+from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton, QGridLayout, QLabel, QLineEdit, QSlider
 
 from copylot.gui._qt.custom_widgets.line_break import QVLineBreakWidget
 
@@ -21,12 +20,16 @@ class LaserDockWidget(QWidget):
         row_index = 0
         parameter_label = QLabel("Laser Power:", self)
         parameter_editbox = QLineEdit(str(0.01), self)
+        parameter_slider = QSlider(Qt.Horizontal)
 
         self.parameters_layout.addWidget(
             parameter_label, row_index, 0, alignment=Qt.AlignTop
         )
         self.parameters_layout.addWidget(
             parameter_editbox, row_index, 1, alignment=Qt.AlignTop
+        )
+        self.parameters_layout.addWidget(
+            parameter_slider, row_index, 2, alignment=Qt.AlignTop
         )
 
         self.main_layout.addLayout(self.parameters_layout)
