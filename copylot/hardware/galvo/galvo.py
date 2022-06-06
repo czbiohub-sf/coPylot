fe509d2ddf2f885e708311bfe780f73ae47263ad
AhmetCanSolak <asolak14@ku.edu.tr>
2022-06-06 14:47:16 -0700

    galvo and laser abstractions initiated

---
+++ copylot/hardware/galvo/galvo.py
@@ -0,0 +1,22 @@
+
+
+class Galvo:
+    speed: float = 0.5
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def scan(self):
+        raise NotImplementedError
+
+    def set_speed(self, s: float):
+        self.speed = s
+
+    def set_minimum(self, min: float):
+        raise NotImplementedError
+
+    def set_maximum(self, max: float):
+        raise NotImplementedError
+
+    def set_position(self, pos: float):
+        raise NotImplementedError
---
+++ copylot/hardware/laser/laser.py
@@ -0,0 +1,22 @@
+
+
+class Laser:
+    def __init__(self, wavelength: int = 488, power: int = 0.15):
+        self._wavelength = wavelength
+        self._power = power
+
+    @property
+    def wavelength(self):
+        return self._wavelength
+
+    @wavelength.setter
+    def wavelength(self, wavelength: int):
+        raise NotImplementedError
+
+    @property
+    def power(self):
+        return self._power
+
+    @power.setter
+    def power(self, power: int):
+        self._power = power
