053c1501e565a99beb53353674123cde53d5b141
acs-ws <asolak@ku.edu.tr>
2022-07-27 14:46:28 -0700

    simulated camera demo added

--- copylot/hardware/orca_camera/simulated_camera.py
+++ copylot/hardware/orca_camera/simulated_camera.py
@@ -1,3 +1,6 @@
+import time
+from os.path import join
+
 import numpy
 
 
@@ -5,7 +8,10 @@ class SimulatedOrcaCamera:
     def __init__(self, camera_index: int = 0):
         self._camera_index = camera_index
 
-    def run(self, nb_frame: int = 100000):
+    def run(self, nb_frame: int = 100000, path=None):
+        if path is None:
+            temp_file = join(get_temp_folder(), "stacks" + str(time.time()))
+
         frames = []
         for _ in nb_frame:
             frames.append(numpy.random.random((2048, 2048)))
