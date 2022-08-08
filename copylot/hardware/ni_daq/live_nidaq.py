3e94ed17abac42b8f71f9b0a8ef2a47af0dd24cb
acs-ws <asolak@ku.edu.tr>
2022-08-08 11:12:57 -0700

    legacy code moved to legacy file

--- copylot/hardware/ni_daq/live_nidaq.py
+++ copylot/hardware/ni_daq/live_nidaq.py
@@ -3,13 +3,28 @@ import nidaqmx
 
 class LiveNIDaq:
     def __init__(self):
-        pass
+        self._active_analog_channels = []
+
+    @property
+    def active_analog_channels(self):
+        """
+        List of active analog channels
+        """
+        return self._active_analog_channels
+
+    @active_analog_channels.setter
+    def active_analog_channels(self, channel):
+        self._active_analog_channels.append(channel)
 
     def zero(self):
-        raise NotImplementedError
+        for channel in self.active_analog_channels:
+            with nidaqmx.Task() as task:
+                task.ao_channels.add_ao_voltage_chan(channel)
+                task.write([0], auto_start=True)
 
     def set_constant_voltage(self, channel, voltage):
         """
+        Sets a given voltage to the given channel.
 
         Parameters
         ----------
@@ -17,6 +32,9 @@ class LiveNIDaq:
         voltage
 
         """
-        with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan(channel)
-            task.write([voltage], auto_start=True)
+        if channel in self.active_analog_channels:
+            with nidaqmx.Task() as task:
+                task.ao_channels.add_ao_voltage_chan(channel)
+                task.write([voltage], auto_start=True)
+        else:
+            raise ValueError("Constant voltage can not be set to an inactive analog channel.")
