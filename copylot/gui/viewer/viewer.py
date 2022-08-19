23b29b09db65e812ff9bae094d467765ed437a01
acs-ws <asolak@ku.edu.tr>
2022-08-19 09:52:43 -0700

    viewer is implemented

--- copylot/gui/viewer/viewer.py
+++ copylot/gui/viewer/viewer.py
@@ -1,17 +1,32 @@
 import numpy
 from vispy import scene
 from vispy import app
-import sys
 
 
 class Viewer:
-    def __init__(self):
-        canvas = scene.SceneCanvas(keys='interactive')
-        canvas.size = 1024, 1024
-        canvas.show()
+    def __init__(self, img_data=None):
+        self.canvas = scene.SceneCanvas(keys='interactive')
+        self.canvas.size = 1024, 1024
+        self.canvas.title = 'Spatial Filtering using %s Filter'
+        self.canvas.show()
 
         # Set up a viewbox to display the image with interactive pan/zoom
-        view = canvas.central_widget.add_view()
+        self.view = self.canvas.central_widget.add_view()
+
+        interpolation = 'nearest'
+
+        self.image = scene.visuals.Image(img_data, interpolation=interpolation,
+                                    parent=self.view.scene, method='subdivide')
+
+        # Set 2D camera (the camera will scale to the contents in the scene)
+        self.view.camera = scene.PanZoomCamera(aspect=1)
+        # flip y-axis to have correct alignment
+        self.view.camera.flip = (0, 1, 0)
+        self.view.camera.set_range()
+        self.view.camera.zoom(0.1, (250, 200))
+
+    def run(self):
+        app.run()
 
     def update(self, data: numpy.ArrayLike):
         pass
