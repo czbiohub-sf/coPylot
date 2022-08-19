82c934f2a74eacce5e9b898deaf7c374b77a7f0c
acs-ws <asolak@ku.edu.tr>
2022-08-19 09:27:28 -0700

    initial canvas business

--- copylot/gui/viewer/viewer.py
+++ copylot/gui/viewer/viewer.py
@@ -1,9 +1,17 @@
 import numpy
+from vispy import scene
+from vispy import app
+import sys
 
 
 class Viewer:
     def __init__(self):
-        pass
+        canvas = scene.SceneCanvas(keys='interactive')
+        canvas.size = 1024, 1024
+        canvas.show()
+
+        # Set up a viewbox to display the image with interactive pan/zoom
+        view = canvas.central_widget.add_view()
 
     def update(self, data: numpy.ArrayLike):
         pass
