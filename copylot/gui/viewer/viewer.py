4d5d4fb0310a97a2d2b33167d39219ebac90e281
acs-ws <asolak@ku.edu.tr>
2022-08-22 10:45:39 -0700

    correct zoom rate

--- copylot/gui/viewer/viewer.py
+++ copylot/gui/viewer/viewer.py
@@ -7,7 +7,7 @@ class Viewer:
     def __init__(self, img_data=None):
         self.canvas = scene.SceneCanvas(keys='interactive')
         self.canvas.size = 1024, 1024
-        self.canvas.title = 'Spatial Filtering using %s Filter'
+        self.canvas.title = 'coPylot viewer'
         self.canvas.show()
 
         # Set up a viewbox to display the image with interactive pan/zoom
@@ -27,7 +27,7 @@ class Viewer:
         # flip y-axis to have correct alignment
         self.view.camera.flip = (0, 1, 0)
         self.view.camera.set_range()
-        self.view.camera.zoom(0.1, (250, 200))
+        self.view.camera.zoom(1, (250, 200))
 
     def run(self):
         app.run()
