67155699c5cb2e83e0fcccc078ba6562d8f521f3
Xiyu Yi <xiyu.yi@czbiohub.org>
2022-11-28 16:53:52 -0800

    try master pulse

--- sandbox/hamamatsu_camera_try_master_pulse.py
+++ sandbox/hamamatsu_camera_try_master_pulse.py
@@ -30,6 +30,7 @@ def dcam_get_properties_name_id_dict(iDevice=0):
     Dcamapi.uninit()
     return name_id_dict
 
+
 def get_first_pixel_sequences():
     first_pixels=[]
     for i0 in  np.arange(1000):
@@ -56,47 +57,72 @@ dcam.is_opened()
 
 # -- try to set exposure time # todo - need to verify if this is working. and think about the order.
 v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.EXPOSURETIME,
-                          fValue=0.1)  # The unit here seems to be in seconds.
-print('exposure time: '+str(v)+' seconds.')
-# -- set external trigger
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERSOURCE,
-                          fValue=4)  # fValue = 4 sets the trigger source to be 'MASTER PULSE'
+                          fValue=0.2)  # The unit here seems to be in seconds.
+print('1 - ' + str(v))
 
+print('exposure time: '+str(v)+' seconds.')
 v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGER_MODE,
-                          fValue=1)  # fValue = 1 sets the trigger mode to be 'NORMAL'.
+                          fValue=3)  # fValue = 1 sets the trigger mode to be 'NORMAL'.
+print('2 - ' + str(v))
+
+dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGER_CONNECTOR, fValue=2)
+
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERACTIVE,
+                      fValue=1)  # 1 set the TRIGGER ACTIVE to be EDGE. (seems like it can only be EDGE, or SYNCREADOUT(3))
+print(v)
+
 
 v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERPOLARITY,
-                          fValue=1)  # fValue = 2 sets the trigger polarity to be 'POSITIVE', 1 to be negative
-
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERTIMES,
-                          fValue=1)  # fValue = 1 sets the trigger times to be 10... find out what it means.
-
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_KIND,
-                          fValue=4)  # fValue = 4 sets the output trigger kind to be TRIGGER READY
-
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_POLARITY,
-                          fValue=1)  # fValue = 2 sets the trigger polarity to be 'POSITIVE'
-
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_BASESENSOR,
-                          fValue=1)  # fValue = 1 sets the output trigger sensor to be 'VIEW 1'.
-
-# -- choose master pulse burst mode
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_MODE,
-                          fValue=2)  # fValue = 3 is burst mode, 1 is continuous mode. 2 is start mode.
-
-# -- set burst mode burst times:
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_BURSTTIMES,
-                          fValue=30)
-
-# -- set burst mode burst times:
-v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_INTERVAL,
-                          fValue=0.01)
-
+                          fValue=2)  # fValue = 2 sets the trigger polarity to be 'POSITIVE', 1 to be negative
 print(v)
+
+
+# -- set master pulse
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERSOURCE,
+                          fValue=4)  # fValue = 4 sets the trigger source to be 'MASTER PULSE'
+print(v)
+
 
 # -- set master pulse trigger to be external trigger:
 v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_TRIGGERSOURCE,
                           fValue=1)  # this sets the trigger source to be external.
+print(v)
+
+
+
+# -- choose master pulse burst mode
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_MODE,
+                          fValue=2)  # fValue = 3 is burst mode, 1 is continuous mode. 2 is start mode.
+print(v)
+
+
+# # -- set burst mode burst times:
+# v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_BURSTTIMES,
+#                           fValue=30)
+
+# -- set burst mode burst times:
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.MASTERPULSE_INTERVAL,
+                          fValue=0.01)
+print(v)
+
+
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.TRIGGERTIMES,
+                          fValue=10)  # fValue = 1 sets the trigger times to be 10... find out what it means.
+
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_KIND,
+                          fValue=4)  # fValue = 4 sets the output trigger kind to be TRIGGER READY
+print(v)
+
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_POLARITY,
+                          fValue=2)  # fValue = 2 sets the trigger polarity to be 'POSITIVE'
+print(v)
+
+v = dcam.prop_setgetvalue(idprop=DCAM_IDPROP.OUTPUTTRIGGER_BASESENSOR,
+                          fValue=1)  # fValue = 1 sets the output trigger sensor to be 'VIEW 1'.
+
+
+
+print(v)
 
 # allocate buffer
 dcam.buf_alloc(200)
