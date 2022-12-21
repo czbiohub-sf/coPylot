0c7d0d9449466200f8a05c6db101bff5dce65fca
AhmetCanSolak <asolak14@ku.edu.tr>
2022-12-21 15:42:33 -0800

    a rough skeleton is implemented

--- copylot/hardware/filters/vt_isim_aotf/vt_isim_aotf.py
+++ copylot/hardware/filters/vt_isim_aotf/vt_isim_aotf.py
@@ -1,3 +1,9 @@
+from enum import Enum
+
+
+class AOTF_MODES(Enum):
+    VTI_AOTF_MANUAL = 0
+    VTI_AOTF_SOFTWARE = 1
 
 
 class AOTF:
@@ -5,11 +11,41 @@ class AOTF:
 
     Parameters
     ----------
+    channel_names : tuple
 
     """
 
-    def __init__(self):
-        raise NotImplementedError()
+    def __init__(
+            self,
+            channel_names: tuple = (),
+    ):
+        self.shutter_on = False
+        self.channel_names = channel_names
+        self.channel_power_status = {
+            channel_name: False for channel_name in channel_names
+        }
 
     def __del__(self):
+        raise NotImplementedError()
+
+    def set_intensity(self, channel_name: str, intensity: int):
+        if channel_name not in self.channel_names:
+            raise ValueError(f"Channel: {channel_name} is not a valid option...")
+
+        if intensity < 0 or intensity > 100:
+            raise ValueError(f"Intensity has to be between [0,100], {intensity} is invalid...")
+
+        # Make a call to vti_SetIntensity()
+        raise NotImplementedError()
+
+    def set_channel_power_status(self, channel_name: str, power: bool = False):
+        if channel_name in self.channel_names:
+            self.channel_power_status[channel_name] = power
+
+            # Make a call to vti_SetTTLBitmask()
+        else:
+            raise ValueError(f"No channel exist with given {channel_name}...")
+
+    def toggle_shutter(self):
+        # Make a call to vti_SetShutter()
         raise NotImplementedError()
