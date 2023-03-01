20a670df6bbefc27308d763890f2db4bcc95bab7
unknown <yang.liu@czbiohub.org>
2023-02-28 17:13:47 -0800

    new demos added

--- copylot/hardware/mirrors/optotune/demo/demo_mirror.py
+++ copylot/hardware/mirrors/optotune/demo/demo_mirror.py
@@ -1,3 +1,7 @@
+from time import sleep
+
+import numpy
+
 from copylot.hardware.mirrors.optotune.mirror import OptoMirror
 
 
@@ -8,13 +12,34 @@ def demo_mirror():
 
     del mirror
 
+
 def demo_xy_scan():
     mirror = OptoMirror()
 
-    print(mirror.positions)
+    for x in numpy.linspace(-0.2, 0.2, 200):
+        mirror.position_x = x
+        sleep(0.03)
+
+    del mirror
+
+
+def demo_spiral_scan():
+    mirror = OptoMirror()
+
+    theta = numpy.linspace(0, 20 * numpy.pi, 500)
+    positions = []
+    for theta in theta:
+        r = theta * .005
+        positions.append((r * numpy.cos(theta), r * numpy.sin(theta)))
+
+    for position in positions:
+        mirror.position_x = position[0]
+        mirror.position_y = position[1]
 
     del mirror
 
 
 if __name__ == '__main__':
     demo_mirror()
+    demo_xy_scan()
+    demo_spiral_scan()
