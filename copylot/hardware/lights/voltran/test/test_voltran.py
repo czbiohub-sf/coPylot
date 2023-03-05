175f11bc015eb8999308680c5c1fe1065bf8ce41
Ivan Ivanov <eduardo.hirata@czbiohub.org>
2023-03-04 22:50:55 -0800

    adding demo file

--- copylot/hardware/lights/voltran/test/test_voltran.py
+++ copylot/hardware/lights/voltran/test/test_voltran.py
@@ -1,7 +1,23 @@
+#%%
 from copylot.hardware.lights.voltran import voltran
 
+def demo_get_laser():
+    laser_list = voltran.get_lasers()
+    print(laser_list)
 
-if __name__ == 'main_':
-    voltran.get_lasers()
-    laser = voltran.VoltranLaser()
-    power = laser.get_power()
+def demo_voltran():
+    laser_list = voltran.get_lasers()
+    laser = voltran.VoltranLaser(port=laser_list[0][0])
+    laser.disconnect()
+
+def demo_send_cmd():
+    laser_list = voltran.get_lasers()
+    print(laser_list[0][0])
+    with voltran.VoltranLaser(port=laser_list[0][0]) as laser:
+        print(laser.laser_power)
+        
+if __name__ == '__main__':
+    demo_get_laser()
+    demo_voltran()
+    demo_send_cmd()
+# %%
