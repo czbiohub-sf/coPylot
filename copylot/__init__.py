ac8f3b5ac9a139af4b9100419bb6c2e14ca2dfe4
acs-home-ws <asolak14@ku.edu.tr>
2023-03-30 16:50:51 -0700

    log formatter updated

--- copylot/__init__.py
+++ copylot/__init__.py
@@ -17,7 +17,7 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 
 # create formatter and add it to the handlers
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s.%(funcName)s - %(message)s')
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
