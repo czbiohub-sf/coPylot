afe907b070d7d8624eb80fdc556972dffe666793
acs-home-ws <asolak14@ku.edu.tr>
2023-03-30 15:26:11 -0700

    file handler added

--- copylot/__init__.py
+++ copylot/__init__.py
@@ -8,13 +8,19 @@ import logging
 logger = logging.getLogger('application')
 logger.setLevel(logging.DEBUG)
 
+# create file handler which logs even debug messages
+fh = logging.FileHandler('log.txt')
+fh.setLevel(logging.DEBUG)
+
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 
 # create formatter and add it to the handlers
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 
+# add the handlers to the logger
+logger.addHandler(fh)
 logger.addHandler(ch)
