0101752185626bfbf6da26e950f06f0dc1827d97
acs-home-ws <asolak14@ku.edu.tr>
2023-03-30 15:21:55 -0700

    initial commit

--- copylot/__init__.py
+++ copylot/__init__.py
@@ -2,3 +2,19 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "not-installed"
+
+import logging
+
+logger = logging.getLogger('application')
+logger.setLevel(logging.DEBUG)
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
