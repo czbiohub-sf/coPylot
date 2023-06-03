9d5d8de6cab393b6fc2c9b7cf5864d98dca206fa
YangLiujames <yang.liu@czbiohub.org>
2023-06-02 17:14:33 -0700

    add closecam function

--- copylot/hardware/cameras/Basler/bCam.py
+++ copylot/hardware/cameras/Basler/bCam.py
@@ -1,8 +1,8 @@
-from pypylon import pylon
+from pypylon import pylon as py
 from pypylon import genicam
 import sys
 
-print('hello world!')
+
 
 exitcode = 0
 
@@ -11,10 +11,10 @@ class bCam:
     def __init__(self):
         # get transport layer and all attached devices
         self.cameras = None
-        self.tl_factory = pylon.TlFactory.GetInstance()
+        self.tl_factory = py.TlFactory.GetInstance()
         self.devices = self.tl_factory.EnumerateDevices()
         if len(self.devices) == 0:
-            raise pylon.RuntimeException("No camera connected")
+            raise py.RuntimeException("No camera connected")
         else:
             self.maxCameraToUSe = len(self.devices)
             print(len(self.maxCameraToUSe))
@@ -28,22 +28,38 @@ class bCam:
 
     def opencam(self):
         try:
-            self.cameras = pylon.InstantCameraArray(min(len(self.devices)),
+            self.cameras = py.InstantCameraArray(min(len(self.devices)),
                                                     self.maxCameraToUSe)  # create multiple camera
             # Create and attach all Pylon Devices.
-            for i, cam in enumerate(self.cameras):
+            for idx, cam in enumerate(self.cameras):
                 cam.Attach(self.tlFactory.CreateDevice(self.devices[i]))
-                cam.Open()
                 # Print the model name of the camera.
                 print("Using device ", cam.GetDeviceInfo().GetModelName())
+                camera_serial = cam.DeviceInfo.GetSerialNumber()
+                print(f"set context {idx} for camera {camera_serial}")
+                cam.SetCameraContext(idx)
+            self.cameras.Open()
+            self.
+
         except genicam.GenericException as e:
             # Error handling
             print("An exception occurred. {}".format(e))
             exitcode = 1
             sys.exit(exitcode)
     def closecam(self):
-        for i, cam in enumerate(self.cameras):
-            cam.Close()
+        try:
+            # Check whether there is any cameras are running and stop the running cam
+            if self.cameras.isGrabbing():
+                for idx, cam in enumerate(self.cameras):
+                    cam.AcquisitionAbort.Execute()
+            #close cam
+            self.cameras.Close()
+        except genicam.GenericException as e:
+            # Error handling
+            print("An exception occurred. {}".format(e))
+            exitcode = 1
+            sys.exit(exitcode)
+
     def trigermode(self,cam):
         cam.Attach(AcquisitionMode.SetValue(AcquisitionMode_SingleFrame);
 )
