e3b9ed3c2cf3f5d3b7c48d6c1f91bc13b0c331f7
Alejandra Duran <alejandra.duran@czbiohub.org>
2023-06-12 13:32:22 -0700

    Adding directory for flir camera adapter

---
+++ copylot/hardware/cameras/flir/flir_camera.py
@@ -0,0 +1,18 @@
+from copylot.hardware.cameras.abstract_camera import AbstractCamera
+
+
+class FlirCameraException(Exception):
+    pass
+
+
+class FlirCamera(AbstractCamera):
+    """
+    Flir Camera DETAILS adapter.
+
+    Parameters
+    ----------
+
+    """
+
+    # any class attributes
+    pass
