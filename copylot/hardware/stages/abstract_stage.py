22b6838e7c26c1cbee5f9dc49437fc840d8fae79
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2023-06-21 11:49:47 -0700

    updating abstract stage class  Implement the abstract class for stages. #173

--- copylot/hardware/stages/abstract_stage.py
+++ copylot/hardware/stages/abstract_stage.py
@@ -1,5 +1,43 @@
-import abc
+from abc import ABCMeta, abstractmethod
 
+class AbstractStage(metaclass=ABCMeta):
 
-class AbstractStage(metaclass=abc.ABCMeta):
-    pass
+    @property
+    @abstractmethod
+    def position(self):
+        "Method to get/set the position in um"
+        raise NotImplementedError()
+    
+    @position.setter
+    @abstractmethod
+    def position(self, value):
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def travel_range(self):
+        """
+        Valid minimum and maximum travel range values.
+        Returns
+        -------
+        Tuple
+            (min_valid_position, max_valid_position)
+        """
+        raise NotImplementedError()
+    
+    @travel_range.setter
+    @abstractmethod
+    def travel_range(self, value):
+        """
+        Set the travel range of the stage
+        ----
+        Tuple
+            (min_valid_position, max_valid_position)
+        """
+    
+    @abstractmethod
+    def move_relative(self, value):
+        " Move the relative distance from current position"
+        raise NotImplementedError()
+    
+    
